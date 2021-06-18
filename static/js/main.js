aa8c2b907dc0af811bd490b8991e20c937b6596a
santipaprika <santi.gj4@gmail.com>
2021-06-19 00:05:02 +0200

    Update content and project animated thumbnails.

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,13 @@
-(function($){
-	$(document).ready(function(){
-		//Empty space. Fill this with your site specific JS code
-	});
+(function ($) {
+    $(document).ready(function () {
+        // Fill this with your site specific JS code
+        $(".project-img").mouseenter(function () {
+            $(this).context.originalImage = $(this).context.attributes.style.value;
+            $(this).context.attributes.style.value = "background-image: url('/static/projects/sun.gif');"
+        })
+
+        $(".project-img").mouseleave(function () {
+            $(this).context.attributes.style.value = $(this).context.originalImage;
+        })
+    });
 })(jQuery);
