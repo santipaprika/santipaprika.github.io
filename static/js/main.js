51ddb4aee4766a7bbbfe51589e218440a21a8e88
santipaprika <santi.gj4@gmail.com>
2021-06-22 22:58:09 +0200

    Revert "Update info, format and projects preview."
    This reverts commit 2ebdc8996faa97caa1e80bc74714c465ea59efbc.

--- static/js/main.js
+++ static/js/main.js
@@ -3,13 +3,11 @@
         // Fill this with your site specific JS code
         $(".project-img").mouseenter(function () {
             $(this).context.originalImage = $(this).context.attributes.style.value;
-            $(this).context.attributes.style.value = $(this).context.dataset.preview;
+            $(this).context.attributes.style.value = "background-image: url('/static/projects/sun.gif');"
         })
 
         $(".project-img").mouseleave(function () {
             $(this).context.attributes.style.value = $(this).context.originalImage;
         })
-
     });
-
 })(jQuery);
