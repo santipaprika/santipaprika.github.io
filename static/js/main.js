de9473b86e05261cee9c4640d31468dd473989da
santipaprika <santi.gj4@gmail.com>
2021-06-22 22:59:08 +0200

    (v2) Update format, content, and previews.

--- static/js/main.js
+++ static/js/main.js
@@ -3,11 +3,13 @@
         // Fill this with your site specific JS code
         $(".project-img").mouseenter(function () {
             $(this).context.originalImage = $(this).context.attributes.style.value;
-            $(this).context.attributes.style.value = "background-image: url('/static/projects/sun.gif');"
+            $(this).context.attributes.style.value = $(this).context.dataset.preview;
         })
 
         $(".project-img").mouseleave(function () {
             $(this).context.attributes.style.value = $(this).context.originalImage;
         })
+
     });
+
 })(jQuery);
